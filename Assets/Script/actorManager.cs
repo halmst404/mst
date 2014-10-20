454c07ae7f9bc9d99583d1ca5ee938c78cdef5f8
CicholDax <cicdax@gmail.com>
2014-10-20 11:29:26 +0900

    最初のコミット

---
+++ Assets/Script/actorManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class actorManager : MonoBehaviour {
+
+    public static Material m_WorldA_Material;    //Aのホログラムの色
+    public Material m_WorldB_Material;
+    public Material m_WorldC_Material;
+    public Material m_WorldD_Material;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //
+    public Material GetlayerMaterial(string Layer)
+    {
+        if( Layer == "worldA" )
+            return m_WorldA_Material;
+        if( Layer == "worldB" )
+            return m_WorldB_Material;
+        if( Layer == "worldC" )
+            return m_WorldC_Material;
+        if( Layer == "worldD" )
+            return m_WorldD_Material;
+
+        return null;
+    }
+}
---
+++ Assets/Script/screenActor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class screenActor : MonoBehaviour {
+
+    public actorManager m_manager;
+
+	// Use this for initialization
+	void Start () {
+
+        Material temp = m_manager.GetlayerMaterial(
+            LayerMask.LayerToName(gameObject.layer));
+        
+
+        //ホログラムを作成
+        CreateHologram( temp );
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+     
+	}
+
+    //holoレイヤーにオブジェクトのコピーを作成
+    private void CreateHologram( Material mat)
+    {
+        GameObject New = new GameObject();
+        New.name = gameObject.name + "(Hologram)";
+        New.gameObject.layer = LayerMask.NameToLayer("holo");
+
+        //メッシュをコピー
+        New.AddComponent<MeshRenderer>();
+        New.AddComponent<MeshFilter>().mesh = gameObject.GetComponent<MeshFilter>().mesh;
+
+        //マテリアルの設定
+        New.renderer.material = mat;
+
+        //親に
+        New.transform.parent = gameObject.transform;
+        New.transform.localPosition = new Vector3(0, 0, 0);
+    }
+}
